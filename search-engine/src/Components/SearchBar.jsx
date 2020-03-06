d389c0292169e9727e6c73bf1c98877026ea2008
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2020-03-06 16:04:23 -0500

    add state and handler events

--- search-engine/src/Components/SearchBar.jsx
+++ search-engine/src/Components/SearchBar.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {Component} from 'react';
 import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
 import '../App.css';
+import { render } from '@testing-library/react';
 
-const SearchBar = () => {
-  return (
-    <MDBCol md="30">
-      <MDBFormInline className="md-form mr-auto mb-4">
-        <input style={{ width: "500px" }} type="text" placeholder="Consulta" aria-label="Search" />
-      </MDBFormInline>
-      <MDBBtn gradient="aqua" rounded size="lg" type="submit" className="mr-auto">
-          Buscar
-        </MDBBtn>
-    </MDBCol>
-  );
+export class SearchBar extends Component{ 
+  state={
+    query : ""
+  }
+
+  handleQuery = (e) =>{
+    console.log("el estado final " ,this.state.query)
+  }
+
+  handleChange = (e) =>{
+    e.preventDefault()
+    this.setState({
+      query: e.target.value
+    });
+    console.log( this.state.query)
+  }
+
+  render(){
+    return (
+      <MDBCol md="30">
+        <MDBFormInline className="md-form mr-auto mb-4">
+          <input style={{ width: "500px" }} type="text" placeholder="Consulta" aria-label="Search" onChange={this.handleChange}/>
+        </MDBFormInline>
+        <MDBBtn gradient="aqua" rounded size="lg" type="submit" className="mr-auto" onClick={this.handleQuery}>
+            Buscar
+          </MDBBtn>
+      </MDBCol>
+    )
+  }
 }
 
 export default SearchBar;
