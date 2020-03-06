281afed9fe6a8675ca7f8ca330fa13ca5e6c674c
Yan Carlos Certuche Grueso <yancertuche@gmail.com>
2020-03-05 19:46:28 -0500

    first prototype

--- search-engine/src/App.js
+++ search-engine/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar.jsx'
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 function App() {
   return (
     <div className="App">
+      <br></br>
       <p>
-        Aqui va el buscador
-        <SearchBar></SearchBar>
+        Buscador Sobre Casos de Aplicación de Ingeniería de Líneas de Producto
       </p>
+      <div className="App">
+      <SearchBar></SearchBar>
+      </div>
     </div>
   );
 }
--- search-engine/src/Components/SearchBar.jsx
+++ search-engine/src/Components/SearchBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
+import '../App.css';
 
 const SearchBar = () => {
   return (
-    <MDBCol md="12">
+    <MDBCol md="30">
       <MDBFormInline className="md-form mr-auto mb-4">
-        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-        <MDBBtn gradient="aqua" rounded size="sm" type="submit" className="mr-auto">
-          Search
-        </MDBBtn>
+        <input className="form-control form-control-sm ml-40 w-75" type="text" placeholder="Consulta" aria-label="Search" />
       </MDBFormInline>
+      <MDBBtn gradient="aqua" rounded size="lg" type="submit" className="mr-auto">
+          Buscar
+        </MDBBtn>
     </MDBCol>
   );
 }
