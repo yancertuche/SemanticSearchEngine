4fc6f46a905fa4c5420df485e412732e9abbe6ce
Yan <yancertuche@gmail.com>
2021-04-12 14:04:14 -0500

    pages

--- search-engine/src/App.js
+++ search-engine/src/App.js
@@ -1,57 +1,32 @@
 import React, {Component} from 'react';
-import './App.css';
-
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-import {I18nProvider, LOCALES} from './i18n';
-import translate from './i18n/translate'
-import CardResult from './Components/CardResult';
-import { MDBBtn, MDBIcon } from "mdbreact";
+import {Result} from "./Pages/Result"
+import {Home} from "./Pages/Home"
 
 export class App extends Component {
-  state = {
-    results : []
-  }
-
-  
-  _handleResults = (results) =>{
-    this.setState({results})
-    console.log("luego de cambiar estado", results)
-  }
   
   render(){
     return (
-      <I18nProvider locale={LOCALES.ESPAÑOL}>
-      <div className="App">
-        <br></br>
-        <div className="Search">
-          <h2 className="Title">
-            {translate('nombre')}
-          </h2>
-          <br></br>
-          <div>
-            <div>
-              <input className = "Input-home" type="text">
-              </input>
-            </div>
-            <br></br>
-            <MDBBtn color="elegant" size="lg" type="submit">
-              <MDBIcon icon="search" className="mr-1" />
-              {translate('buscar')} 
-            </MDBBtn>
-          </div>
-          <br></br>
-          {this.state.results.length === 0 
-          ?<h3>
-            {translate('por')} Yan Carlos Certuche Grueso
-            </h3>
-          : <CardResult crd = {this.state.results}></CardResult> }
-        </div>
-      </div>
-      </I18nProvider>
+      <Router>
+        <Switch>
+          <Route path='/' component={Home}>
+          </Route>
+          <Route path='/q' component={Result}>
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
