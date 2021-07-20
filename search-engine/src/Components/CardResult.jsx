c0987e1c1313147b1e0f837a0eac68d7e489ebff
Yan <yancertuche@gmail.com>
2021-07-20 15:21:21 -0500

    fixing security issue

--- search-engine/src/Components/CardResult.jsx
+++ search-engine/src/Components/CardResult.jsx
@@ -22,7 +22,7 @@ export class CardResult extends Component{
                     :<Card className="cardInside">
                         {Object.entries(currency).map( label => (
                             label[0] === "url"
-                            ? <a href={label[1].value} target="_blank">
+                            ? <a href={label[1].value} target="_blank" rel="noopener noreferrer">
                                 <Card.Text>Clic aqui para ir a la fuente</Card.Text>
                             </a>
                             :<Card.Text><b>{label[0]}</b> : {label[1].value}</Card.Text>
