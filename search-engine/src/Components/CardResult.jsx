b3c350df0c44d34b95a397b8afb985ae3ce38a88
Yan <yancertuche@gmail.com>
2021-07-25 23:11:46 -0500

    split in titles of card

--- search-engine/src/Components/CardResult.jsx
+++ search-engine/src/Components/CardResult.jsx
@@ -21,11 +21,11 @@ export class CardResult extends Component{
                     </Card>
                     :<Card className="cardInside">
                         {Object.entries(currency).map( label => (
-                            label[0] === "url"
+                            label[0] === "url" || label[0] === "UrlObjRelated"
                             ? <a href={label[1].value} target="_blank" rel="noopener noreferrer">
                                 <Card.Text>Clic aqui para ir a la fuente</Card.Text>
                             </a>
-                            :<Card.Text><b>{label[0]}</b> : {label[1].value}</Card.Text>
+                            :<Card.Text><b>{label[0].split(/(?=[A-Z])/).join(" ")}</b> : {label[1].value}</Card.Text>
                         ))}
 
                         {/*<Card.Text>{JSON.stringify(currency.Uri.value)}</Card.Text>
