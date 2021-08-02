e86903ae70c425083775496b8688d26a649f8fbf
Yan <yancertuche@gmail.com>
2021-08-02 00:07:09 -0500

    passing language to navbar Component

--- search-engine/src/Components/Sidebar.jsx
+++ search-engine/src/Components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -8,65 +8,69 @@ import {
   CDBSidebarMenuItem,
 } from 'cdbreact';
 import { NavLink } from 'react-router-dom';
+import { I18nProvider } from '../i18n';
+import translate from '../i18n/translate';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
   return (
-    <div
-      style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
-    >
-      <CDBSidebar textColor="#fff" backgroundColor="#333">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a
-            href="/"
-            className="text-decoration-none"
-            style={{ color: 'inherit' }}
-          >
-            Dashboard
-          </a>
-        </CDBSidebarHeader>
-
-        <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu>
-            <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Principal</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/graphic" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Explorar Datos</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/result" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="search">Búsqueda avanzada</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">
-                Analytics
-              </CDBSidebarMenuItem>
-            </NavLink>
-
-            {/*<NavLink
-              exact
-              to="/hero404"
-              target="_blank"
-              activeClassName="activeClicked"
+    <I18nProvider locale={props.la}>
+        <div
+        style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
+        >
+        <CDBSidebar textColor="#fff" backgroundColor="#333">
+            <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+            <a
+                href="/"
+                className="text-decoration-none"
+                style={{ color: 'inherit' }}
             >
-              <CDBSidebarMenuItem icon="exclamation-circle">
-                404 page
-              </CDBSidebarMenuItem>
-            </NavLink>*/}
-          </CDBSidebarMenu>
-        </CDBSidebarContent>
+                Dashboard
+            </a>
+            </CDBSidebarHeader>
 
-        <CDBSidebarFooter 
-        style={{ textAlign: 'center' }}>
-          <div
-            style={{
-              padding: '18px 5px',
-            }}
-          >
-            Yan
-          </div>
-        </CDBSidebarFooter>
-      </CDBSidebar>
-    </div>
+            <CDBSidebarContent className="sidebar-content">
+            <CDBSidebarMenu>
+                <NavLink exact to="/" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="columns">Principal</CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink exact to="/graphic" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="chart-line">Explorar Datos</CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink exact to="/result" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="search">Búsqueda avanzada</CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink exact to="/analytics" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="user">
+                    Analytics
+                </CDBSidebarMenuItem>
+                </NavLink>
+
+                {/*<NavLink
+                exact
+                to="/hero404"
+                target="_blank"
+                activeClassName="activeClicked"
+                >
+                <CDBSidebarMenuItem icon="exclamation-circle">
+                    404 page
+                </CDBSidebarMenuItem>
+                </NavLink>*/}
+            </CDBSidebarMenu>
+            </CDBSidebarContent>
+
+            <CDBSidebarFooter 
+            style={{ textAlign: 'center' }}>
+            <div
+                style={{
+                padding: '18px 5px',
+                }}
+            >
+                Yan
+            </div>
+            </CDBSidebarFooter>
+        </CDBSidebar>
+        </div>
+    </I18nProvider>
   );
 };
 
